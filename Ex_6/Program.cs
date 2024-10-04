d1daceb54d2f7837e45b48e0a95fa1c6b3132ba6
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:38:31 +0300

    Add project files.

---
+++ Ex_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть кількість років: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 12 && age <= 18)
+        {
+            Console.WriteLine("Ці роки є підлітковими.");
+        }
+        else
+        {
+            Console.WriteLine("Це не підліткові роки.");
+        }
+    }
+}
